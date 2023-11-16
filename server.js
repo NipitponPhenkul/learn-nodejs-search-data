2b83dcd8ec873eb4b6a78a28e7528f67278500b0
NipitponPhenkul <nipitponphenkul@hotmail.com>
2023-11-16 14:42:25 +0700

    Init project

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const path = require('path')
+const { MongoClient } = require('mongodb')
+
+const app = express()
+const client = new MongoClient('mongodb://localhost')
+const col = client.db('mflix').collection('movies')
+
+client.connect().catch(console.error)
+
+app.set('view engine', 'pug')
+app.set('views', path.join(__dirname, './views'))
+
+app.get('/', async (req, res) => {
+  const page = req.query.page ? Number(req.query.page) : 1
+  const limit = 20
+  const movies = await col
+    .find()
+    .skip((page -1) * limit)
+    .limit(limit)
+    .toArray()
+  const total = await col.countDocuments()
+  const totalPage = Math.ceil(total / limit)
+  res.render('index', { movies, total, page, limit, totalPage })
+})
+
+app.listen(3000, () => {
+  console.log('App started at http://localhost:3000/')
+})
