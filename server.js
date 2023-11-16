477356cc54bfe08da7c487582a044c078d22680e
NipitponPhenkul <nipitponphenkul@hotmail.com>
2023-11-16 15:47:09 +0700

    Add search support

--- server.js
+++ server.js
@@ -14,18 +14,24 @@ app.set('views', path.join(__dirname, './views'))
 app.get('/', async (req, res) => {
   const page = req.query.page ? Number(req.query.page) : 1
   const limit = 20
-  const movies = await col
-    .aggregate([
-      { $skip: (page -1) * limit },
-      { $limit: limit }
-    ]).toArray()
-    // .find()
-    // .skip((page -1) * limit)
-    // .limit(limit)
-    // .toArray()
-  const total = await col.countDocuments()
+  const keyword = req.query.keyword ? { title: { $regex: req.query.keyword }} : {}
+  const pipeline = [
+    { $match: keyword},
+    { $project: { title: 1, type: 1, languages: 1, released: 1 }},
+    { $skip: (page -1) * limit },
+    { $limit: limit }
+  ]
+  pipeline.unshift()
+  const movies = await col.aggregate(pipeline).toArray()
+  const total = await col.countDocuments(keyword)
   const totalPage = Math.ceil(total / limit)
-  res.render('index', { movies, total, page, limit, totalPage })
+  res.render('index', {
+    keywordInput: req.query.keyword,
+    movies,
+    total,
+    page,
+    limit,
+    totalPage })
 })
 
 app.listen(3000, () => {
